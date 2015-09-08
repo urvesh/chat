c2083d965cb4b6722fe6b82bbc49eef7d9e8b734
Urvesh <urveshpatel94@yahoo.com>
2015-09-07 21:36:55 -0400

    fixed message not showing up on view

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ io.on('connection', function(socket) {
   });
 
 
-  socket.on('chat message', function(person, msg) {
-    console.log('message:', msg, person);
+  socket.on('chat message', function(msg) {
+    console.log('message:', msg);
     io.emit('chat message', msg);
   });
 
